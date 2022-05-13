ff1645cffe5a31c038ed9016b0ba58d25a5ec088
lin2333 <2567165156@qq.com>
2022-05-13 11:00:57 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="app">
+        <router-view />11
+    </div>
+</template>
+
+<script>
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+      <Header />123
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue"
+export default {
+  name:'Home',
+  components:{
+    Header
+  }
+}
+</script>
+
+<style>
+
+</style>
